feb777b41ed1b5a16c2a4d23528b065698a67016
BDDr56767 <ethanalex129@gmail.com>
2025-05-16 17:00:26 +0200

    First I/0 function with documentation
    Added the "parse_uploaded_file(file)" function which will allow users to upload a file to the shiny app

---
+++ R/data_io.R
@@ -0,0 +1,20 @@
+library(roxygen2)
+library(shiny)
+library(readxl)
+#Description: a scrip for data input, output and parsing for the shiny app.
+
+#'Parse uploaded Excel file
+#'
+#' @param file An input file for shiny
+#' @return A data.frame
+#' @export
+parse_uploaded_file <- function(file) {
+  req(file)
+  ext <- tools::file_ext(file$datapath)
+  if (ext %in% c("xls", "xlsx")) {
+    readxl::read_excel(file$datapath)
+  } else {
+    stop("Unsupported file type")
+  }
+}
+
